4079cf99f2231e52fbf0a4a0345702839e545733
HKMV <1360359624@qq.com>
2026-05-31 00:48:54 +0800

    修复升级依赖导致的问题

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,8 +1,9 @@
+import { mount } from 'svelte'
 import 'uno.css'
 import App from './App.svelte'
 import '../app.css'
 
 window.addEventListener('error', e => console.error('[API Analyzer]', e.message, e.error))
 
-const app = new App({ target: document.getElementById('app')! })
+const app = mount(App, { target: document.getElementById('app')! })
 export default app
